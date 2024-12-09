c5781acfb4630709da0f7b8911af1236bb69d6b5
Ocyrus47 <omin.limbu@hotmail.com>
2024-12-09 21:28:12 +0000

    Add code to add 2 random numbers for game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,15 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 })
 
+/**This main game loop, callend when the script is first loaded
+ * and after the users answers has been processed.
+ */
+
 function runGame() {
 
+    //creates 2 x random number between 1 - 25
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 }
 
 function checkAnswer() {
