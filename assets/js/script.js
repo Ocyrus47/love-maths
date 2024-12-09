232505b0976007cea26878705fe9c1a7a0a5c69f
Ocyrus47 <omin.limbu@hotmail.com>
2024-12-09 21:13:41 +0000

    Add event listener for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if(this.getAttribute('data-type') === 'submit') {
+                alert('you clicked submit!');
+            } else {
+                let gameType = this.getAttribute('data-type');
+                alert(`you clicked ${gameType}`);
+            }
+
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -27,5 +43,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
